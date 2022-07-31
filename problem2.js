c3d224651aaeb7090a48fcc5f07c4c29832dbc73
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-07-31 23:18:30 +0600

    Problem-2 is solved

--- problem2.js
+++ problem2.js
@@ -12,3 +12,24 @@ Print the result in 2 decimal places.
 */
 
 // Write your program here.
+
+// Declaring necessary variable
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+
+// sum the total number of subjects
+var subjects = 5
+// sum the total marks
+var totalMarks = math +  biology + chemistry + physics + bangla;
+
+// getting average of marks
+var average = totalMarks / subjects;
+
+// getting average for 2 decimal places
+var result = average.toFixed(2)
+
+// console.log(result);
