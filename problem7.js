f3f055064b537026f16b4e523c1ca6cb30f7ae99
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-08-01 23:09:56 +0600

    Problem-7 is solved

--- problem7.js
+++ problem7.js
@@ -8,3 +8,32 @@
 */
 
 // Write your program here.
+
+// ###### Solutions-1 ########
+
+// initial variable for marks
+var num1 = 13;
+var num2 = 79;
+var num3 = 45;
+
+
+// Here I am finding the largest number.
+if (num1 > num2 && num1 > num3) {
+   console.log(`The largest number is: ${num1}`);
+} else if(num2 > num1 && num2 > num3) {
+   console.log(`The largest number is: ${num2}`);
+} else{
+   console.log(`The largest number is: ${num3}`);
+}
+
+// ===================================
+// ###### Solutions-2 ########
+
+// initial variable for sides
+var height = 9;
+var land = 8;
+
+// Here is using the Theory of Triangle
+var triangleArea = (land * height) / 2;
+
+console.log(`Triangle Area is: ${triangleArea}`);
