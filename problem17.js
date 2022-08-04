c7b6e11f01c3a68cee101964124719a8a96fbad4
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-08-04 22:30:16 +0600

    Problem-17 is solved

---
+++ problem17.js
@@ -0,0 +1,14 @@
+/*
+ 1. Write a function that will take an array with 1-20 numbers randomly and will 
+    arrange those numbers chronologically.
+*/
+
+
+const myArr2 = [5, 6, 2, 4, 7, 9, 1, 3, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+myArr2.sort(function(a,b){
+    return a-b
+});
+
+console.log(myArr2);
+
