ddfd61eb8782bb187da02634214064db11e60c65
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-08-04 13:42:49 +0600

    Problem-16 solved

---
+++ problem16.js
@@ -0,0 +1,18 @@
+/*
+ 1. Write a function that will take a number and return the square of 
+    this number.
+*/
+
+// Write your program here.
+
+// #### Solution ####
+
+function square(number) {
+    const gerSquare = number * number
+    console.log(gerSquare);
+    return gerSquare
+}
+square(55)
+square(10)
+square(15)
+square(20)
