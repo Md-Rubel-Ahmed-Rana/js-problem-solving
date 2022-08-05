3c4fb9d869aa5498ccc6750307c805de91ad94f2
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-08-06 00:20:31 +0600

    Problem-25 is solved

--- problem025.js
+++ problem025.js
@@ -1,6 +1,6 @@
 /*
   1. Write a function that will return those numbers which are dividev by 3 and 5.
-  2. Write a loop that will give numbers from 1-100.
+  2. Write a loop inside the function that will give numbers from 1-100.
 
 */
 
@@ -9,14 +9,11 @@
 // #### Solution ####
 
 function getResult() {
-    let result = 0;
     for (let i = 1; i < 100; i++) {
        if(i % 3 == 0 && i % 5 == 0){
-         result = i;
          console.log(i, "is divied by 3 and 5");
        }
     }
-    return result
 }
 
 getResult()
